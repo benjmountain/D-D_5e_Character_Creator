be14bf9dd0b1a3c3e2d313d6a02e4fe917a47a9b
benjmountain <benj.mountain@gmail.com>
2020-05-05 10:39:26 -0400

    Fix db location

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_20_211657) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "player_name"
     t.string "name"
